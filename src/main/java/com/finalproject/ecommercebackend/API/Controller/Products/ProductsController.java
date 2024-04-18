81a0112bc1617341a7e1e54e8cabbc9f4e6d1c37
Low3ee <louieabad000@gmail.com>
2024-04-19 05:51:01 +0800

    added basic endpoint security

--- src/main/java/com/finalproject/ecommercebackend/API/Controller/Products/ProductsController.java
+++ src/main/java/com/finalproject/ecommercebackend/API/Controller/Products/ProductsController.java
@@ -2,16 +2,21 @@ package com.finalproject.ecommercebackend.API.Controller.Products;
 
 import com.finalproject.ecommercebackend.Models.Product;
 import com.finalproject.ecommercebackend.Service.ProductService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/product")
+@RequestMapping("/products")
 public class ProductsController {
     private ProductService productService;
 
+    public ProductsController(ProductService productService) {
+        this.productService = productService;
+    }
+    @GetMapping
     public List<Product> getProducts(){
         return productService.getProducts();
     }
